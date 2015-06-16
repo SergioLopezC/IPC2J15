39579cb3ab37c7f410dba92fe6c089e76ed4741f
SergioLopezC <sergio.lopez_j24@hotmail.com>
2015-06-16 20:02:55 +0200

    CREACION DE  TABLAS
    agregardo mas tablas factura, detalle, producto ,impuesto,asignacion

--- PROYECTO FASE 2/ProyectoFase2.sql
+++ PROYECTO FASE 2/ProyectoFase2.sql
@@ -30,8 +30,39 @@ tel_sucursal numeric(15,0) not null,
 cant_empl_sucursal int not null
 )
 create table cliente(
-cod_cliente char(5)
+cod_cliente char(5) not null,
+CUI_cliente varchar(30)not null,
+NIT_cliente varchar(30)not null,
+nom_cliente varchar(30)not null,
+apell_cliente varchar(30)not null,
+tel_cliente numeric(15,0)not null,
+dir_cliente varchar(50)not null,
+)
+create table tarjeta(
+cod_tarjeta char(16)not null,
+tipo_tajeta varchar(30) not null,
+)
+create table impuesto(
+cod_impuesto char(5) not null,
+porcentaje_impuesto int not null,
+descripcion_impuesto varchar(30) null
+)
 
+create table factura(
+cod_factura char(5) not null,
+nom_empresa varchar(30) not null,
+monto int not null
+)
+create table detalle(
+cod_detalle char(5) not null
+)
+create table producto(
+cod_producto char(5) not null,
+nom_producto varchar(30) not null,
+desc_producto varchar(30) null,
+peso_producto numeric(5,2) not null
+)
 
-
+create table asignacion(
+cod_asignacion char(5) not null,
 )
