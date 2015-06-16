688c0b87b312ced661c166d6d30ab70b13822acb
SergioLopezC <sergio.lopez_j24@hotmail.com>
2015-06-16 20:47:23 +0200

    CREACION DE WEB SERVICE Y LA APLICACION WEB CLIENTE
    creacionn del web service y la aplicacion cliente de nuestro proyecto

---
+++ PROYECTO FASE 2/WebService/WebServiceQuetzalExpress/WebServiceQuetzalExpress/Service1.asmx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebServiceQuetzalExpress
+{
+    /// <summary>
+    /// Summary description for Service1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/",Name="QUETZAL EXPRESS")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Service1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+        [WebMethod]
+        public string ingresoPaquetes()
+        {
+            return "INGRESO DE PAQUETES";
+        }
+        [WebMethod]
+        public string facturacionPaquetes()
+        {
+            return "FACTURACION DE ENVIO";
+        }
+        [WebMethod]
+        public string listadoCliente()
+        {
+            return "LISTADO DE CLIENTES";
+        }
+    }
+}
