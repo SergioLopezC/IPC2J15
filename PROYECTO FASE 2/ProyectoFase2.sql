aedc75c49c8511e6464b0b808c73b0dd9956830b
SergioLopezC <sergio.lopez_j24@hotmail.com>
2015-06-16 10:41:44 +0200

    CREACION DE TABLAS
    Creacion de tabla de la base de datos quetzal express

--- PROYECTO FASE 2/ProyectoFase2.sql
+++ PROYECTO FASE 2/ProyectoFase2.sql
@@ -1,2 +1,37 @@
 create database ProyectoFase2
 go
+use ProyectoFase2
+--creacion de tablas sistema quetzal express
+create table empleado(
+cod_empleado char(5) not null,
+nom_empleado varchar(30) not null,
+apell_empleado varchar(30)not null,
+CUI_empleado varchar(30)not null,
+tel_empleado numeric(15,0)not null,
+sueldo_empleado int not null,
+dir_empleado varchar(50)not null
+)
+create table departamento(
+cod_departamento char(5) not null,
+nom_departamento varchar(30) not null,
+desc_departamento varchar(50) null,
+nom_director varchar(30) not null,
+apell_director varchar(30)not null
+)
+create table sede(
+cod_sede char(5) not null,
+dir_sede varchar(50) not null
+)
+create table sucursal(
+cod_sucursal char(5) not null,
+pais_sucursal varchar(30) not null,
+dir_sucursal varchar(50) not null,
+tel_sucursal numeric(15,0) not null,
+cant_empl_sucursal int not null
+)
+create table cliente(
+cod_cliente char(5)
+
+
+
+)
