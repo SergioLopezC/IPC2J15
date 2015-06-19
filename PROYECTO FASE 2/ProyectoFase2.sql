605e460f343cc5463b61075416d4eee5bd401d6b
SergioLopezC <sergio.lopez_j24@hotmail.com>
2015-06-19 05:54:31 -0600

    MODIFICACION TABLAS SEGUNDA FASE
    se modifico las  tablas anteriores para la  segunda fase del proyecto

--- PROYECTO FASE 2/ProyectoFase2.sql
+++ PROYECTO FASE 2/ProyectoFase2.sql
@@ -5,11 +5,11 @@ use ProyectoFase2
 create table empleado(
 cod_empleado char(5) not null,
 nom_empleado varchar(30) not null,
+puesto_empleado varchar(30) not null,
 apell_empleado varchar(30)not null,
-CUI_empleado varchar(30)not null,
 tel_empleado numeric(15,0)not null,
 sueldo_empleado int not null,
-dir_empleado varchar(50)not null
+dir_empleado varchar(50)not null,
 )
 create table departamento(
 cod_departamento char(5) not null,
@@ -20,7 +20,8 @@ apell_director varchar(30)not null
 )
 create table sede(
 cod_sede char(5) not null,
-dir_sede varchar(50) not null
+dir_sede varchar(50) not null,
+pais_sede varchar(50) not null
 )
 create table sucursal(
 cod_sucursal char(5) not null,
@@ -31,7 +32,7 @@ cant_empl_sucursal int not null
 )
 create table cliente(
 cod_cliente char(5) not null,
-CUI_cliente varchar(30)not null,
+DPI_cliente varchar(30)not null,
 NIT_cliente varchar(30)not null,
 nom_cliente varchar(30)not null,
 apell_cliente varchar(30)not null,
@@ -44,6 +45,7 @@ tipo_tajeta varchar(30) not null,
 )
 create table impuesto(
 cod_impuesto char(5) not null,
+tipo_impuesto varchar(30) not null,
 porcentaje_impuesto int not null,
 descripcion_impuesto varchar(30) null
 )
@@ -51,6 +53,7 @@ descripcion_impuesto varchar(30) null
 create table factura(
 cod_factura char(5) not null,
 nom_empresa varchar(30) not null,
+serie_factura varchar(20) not null,
 monto int not null
 )
 create table detalle(
@@ -60,9 +63,24 @@ create table producto(
 cod_producto char(5) not null,
 nom_producto varchar(30) not null,
 desc_producto varchar(30) null,
+precio_producto numeric(7,2) not null,
 peso_producto numeric(5,2) not null
 )
 
-create table asignacion(
-cod_asignacion char(5) not null,
+create table paquete(
+cod_paquete char(5) not null
 )
+
+create table lote(
+cod_lote char(5) not null,
+fecha_envio datetime not null
+)
+create table estado_paquete(
+cod_estado char(5) not null,
+estado varchar(30) not null,
+fecha datetime not null
+)
+create table bodega(
+cod_bodega char(5) not null
+)
+
